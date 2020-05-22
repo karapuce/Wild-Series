aa3dd3972b50843c1056894d0a9fcd4dd80f4786
karapuce <typhaine.demangeon@gmail.com>
2020-05-22 16:42:56 +0200

    quest symfony

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index")
+     * @return Response
+     */
+    public function index() :Response
+    {
+        return $this->render('home.html.twig');
+    }
+
+}
---
+++ src/Controller/WildController.php
@@ -0,0 +1,40 @@
+<?php
+// src/Controller/WildController.php
+namespace App\Controller;
+
+use phpDocumentor\Reflection\Types\Mixed_;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/wild", name="wild_")
+ */
+class WildController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('wild/index.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+
+    /**
+     * @Route("/show/{slug}",
+     *     requirements={"slug"="[a-z0-9-]+"},
+     *     defaults={"slug"="Aucune série sélectionnée, veuillez choisir une série"},
+     *     name="show_slug")
+     * @param string $slug
+     * @return Response
+     */
+    public function show($slug): Response
+    {
+        $slug = str_replace("-"," ",$slug);
+
+        return $this->render('wild/show.html.twig', ['slug' => ucwords($slug)]);
+    }
+
+}
