b6f031d86a652a841efbafc5042cd7ed349ffba6
karapuce <typhaine.demangeon@gmail.com>
2020-06-15 17:14:23 +0200

    c'est long j'en ai marre j'ai faim

--- src/Controller/CatController.php
+++ src/Controller/CatController.php
@@ -9,8 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/category", name="category_")
@@ -36,8 +35,8 @@ class CatController extends AbstractController
     /**
      * @Route ("/add",name="add")
      * @param Request $request
-     * @param EntityManagerInterface $em
      * @return Response
+     * @IsGranted("ROLE_ADMIN")
      */
     public function add(Request $request){
 
